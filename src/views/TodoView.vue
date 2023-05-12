fcdccddc643cc7b6afc5844349e7000ff75037ad
shahab Ramzan <96526562+shahab-ramzan@users.noreply.github.com>
2023-05-12 22:23:09 +0500

    todo added

--- src/views/TodoView.vue
+++ src/views/TodoView.vue
@@ -1,3 +1,23 @@
+<script setup>
+import Todo from '../components/Todo.vue'
+</script>
 <template>
-  <h1>Todo Page</h1>
+  <main>
+    <div class="todo">
+      <h1>This is an Todo page</h1>
+      <Todo />
+    </div>
+  </main>
 </template>
+
+<style>
+@media (min-width: 1024px) {
+  .todo {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    justify-content: center;
+  }
+}
+</style>
